d34d763efeaa7ed06e869bcadec73ca33c782e6a
fanadape <fanadape@gmail.com>
2023-10-16 12:44:57 -0700

    chore: add files css

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,10 @@
 <template>
-	<div class="sidebar"></div>
+	<div class="sidebar">
+		<NuxtLink class="logo" to="/">
+			<img class="logo__image" src="@/assets/svgs/logo.svg" alt="logo" />
+			<span class="logo__text">To Do Manager</span>
+		</NuxtLink>
+	</div>
 	<div class="container">
 		<slot />
 	</div>
@@ -7,4 +12,18 @@
 
 <script lang="ts" setup></script>
 
-<style></style>
+<style scoped>
+.sidebar {
+	max-width: 288px;
+}
+
+.logo {
+	display: flex;
+	align-items: center;
+	column-gap: 20px;
+
+	text-decoration: none;
+	color: #120e21;
+	font-weight: 700;
+}
+</style>
